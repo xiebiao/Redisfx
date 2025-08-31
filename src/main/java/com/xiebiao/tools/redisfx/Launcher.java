256a1d1f9fb6238f9ac6efcfbef5c0ba5d987fdd
biaoxie <joyrap@gmail.com>
2025-08-31 20:07:18 +0800

    添加maven-shade-plugin打包

---
+++ src/main/java/com/xiebiao/tools/redisfx/Launcher.java
@@ -0,0 +1,11 @@
+package com.xiebiao.tools.redisfx;
+
+/**
+ * @author Bill Xie
+ * @since 2025/8/31 13:58
+ **/
+public class Launcher {
+    public static void main(String[] args) {
+        RedisfxApplication.main(args);
+    }
+}
